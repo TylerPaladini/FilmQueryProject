936286611a608fdf49c9f7ebf2ec1ed224df2f34
Tyler Paladini <TylerPaladini@yahoo.com>
2018-10-08 08:19:41 -0600

    final project w/o stretch goals

--- src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
+++ src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
@@ -18,31 +18,39 @@ public class DatabaseAccessorObject implements DatabaseAccessor {
 		Connection conn = DriverManager.getConnection(URL, user, pass);
 
 		Film film = null;
-//		Actor actor = null;
+		Actor actor = null;
 		
-		String sql = "select id, title, description, release_year, language_id, "
-				+ "rental_duration, rental_rate, length, replacement_cost, rating, "
-				+ "special_features from film WHERE id = ?";
+		String sql = "select film.id, title, description, release_year, language.name, "
+				+ "rental_duration, rental_rate, length, replacement_cost, rating,special_features,actor.id, actor.first_name, actor.last_name  "
+				+ "from film "
+				+ "join language "
+				+ "on film.language_id = language.id "
+				+ "join film_actor on film.id = film_actor.film_id "  
+			    + "  join actor on actor.id = film_actor.actor_id "
+				+ " WHERE film.id = ?";
 		PreparedStatement stmt = conn.prepareStatement(sql);
 		stmt.setInt(1, filmId);
 
 		ResultSet filmResult = stmt.executeQuery();
 		if (filmResult.next()) {
 			film = new Film();
-//			actor = new Actor();
+			actor = new Actor();
 			
 			film.setId(filmResult.getInt("id"));
 			film.setTitle(filmResult.getString("title"));
 			film.setDescription(filmResult.getString("description"));
 			film.setRelease_year(filmResult.getInt("release_year"));
-			film.setLanguage_id(filmResult.getString("language_id"));
+			film.setLanguage_id(filmResult.getString("language.name"));
 			film.setRentalDuration(filmResult.getInt("rental_duration"));
 			film.setRentalRate(filmResult.getDouble("rental_rate"));
 			film.setLength(filmResult.getInt("length"));
 			film.setReplacementCost(filmResult.getDouble("replacement_cost"));
 			film.setRating(filmResult.getString("rating"));
 			film.setSpecialFeature(filmResult.getString("special_features"));
-				
+			actor.setId(filmResult.getInt("id"));
+        	actor.setFirst_name(filmResult.getString("first_name"));
+        	actor.setLast_name(filmResult.getString("last_name"));
+        film.setActor(actor);
 				
             
 
